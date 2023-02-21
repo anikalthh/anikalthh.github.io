f56d478b4960c352ee222c7fb854d039e6ebee03
anikalthh <95908881+anikalthh@users.noreply.github.com>
2023-02-21 13:12:36 +0800

    HashRouter changed again

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import HomePage from './components/HomePage.jsx';
 import Contact from './components/Contact.jsx';
 import Navigation from './components/Navbar.jsx';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Experience from './components/Experience.jsx';
 
 function App() {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import TopContainer from './TopContainer.jsx';
+import { Link } from "react-router-dom";
 
 function Navigation() {
     return (
@@ -11,9 +11,12 @@ function Navigation() {
             <Navbar.Brand className="Nav-text" href="/">ANI</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Nav className='nav'>
-                <Nav.Link className="Nav-text" href="./#about" onClick={TopContainer.handleClick}>ABOUT ME</Nav.Link>
-                <Nav.Link className="Nav-text" href="./Experience">EXPERIENCE</Nav.Link>
-                <Nav.Link className="Nav-text" href="./Contact">SAY HELLO</Nav.Link>
+                <Nav.Link className="Nav-text" as={Link} to="./#about" onClick={() => {
+                                                            const element = document.getElementById("about");
+                                                            element.scrollIntoView({behavior: 'smooth'});
+                                                            }}>ABOUT ME</Nav.Link>
+                <Nav.Link className="Nav-text" as={Link} to="./Experience">EXPERIENCE</Nav.Link>
+                <Nav.Link className="Nav-text" as={Link} to="./Contact">SAY HELLO</Nav.Link>
             </Nav>
         </Container>
     </Navbar>)
